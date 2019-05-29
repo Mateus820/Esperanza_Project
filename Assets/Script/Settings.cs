1df6b7a79ceba6355fa5204880fcc93556e0d159
Aluno <aluno@naverecife.local>
2019-05-29 08:22:33 -0300

    initial scene

---
+++ Assets/Script/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour {
+
+	public AudioSource source;
+	public  AudioClip [] clips;
+
+	private void Start() {
+		
+	}
+
+	public void Mute(string name)
+	{
+		AudioListener.pause = true; 
+	}
+
+	public void UnMute(){
+		
+	}
+}
